22d8db6370f3147c8c1f30cbc38e6f42bde754da
doc2288 <tata6602tata@gmail.com>
2025-09-27 02:17:38 +0300

    Create level_loader.py

---
+++ src/level_loader.py
@@ -0,0 +1,17 @@
+class LevelLoader:
+    """Handles loading and parsing game levels."""
+    
+    def __init__(self):
+        self.current_level = None
+        
+    def load_level(self, level_file):
+        """Load a level from file."""
+        pass
+        
+    def parse_level_data(self, data):
+        """Parse level data and return level objects."""
+        pass
+        
+    def get_spawn_points(self):
+        """Get player and enemy spawn points."""
+        pass
